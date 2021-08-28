08c6486db67e0897541f499f51c9c616b77a670d
Prakash Gupta <pgupta@netsuite.com>
2021-08-28 15:54:49 -0400

    first commit

---
+++ App.ts
@@ -0,0 +1,18 @@
+let message : string = "Hello";
+let var1 = `"hello"`;
+let car : any = {
+    "make": "Honda",
+    "year": "2018"
+}
+console.log(message);
+console.log(message);
+
+function sum(p1, p2){
+    return p1 + p2;
+}
+
+console.log(sum(2,3));
+
+console.log(car.make);
+console.log(var1);
+
